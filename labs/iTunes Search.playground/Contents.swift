f04cc476449d2ba18973a15ab618e7cc8e1db66e
Paul Stewart <paul.stewart.professional@gmail.com>
2023-11-30 09:58:34 -0700

    iTunes works (yippee)

--- labs/iTunes Search.playground/Contents.swift
+++ labs/iTunes Search.playground/Contents.swift
@@ -1,32 +1,15 @@
+
 import UIKit
-import Foundation
 
-struct ReturnedMusic: Codable {
-    var artistName: String
-    var trackName: String
-}
-
-enum networkError: Error {
-    case httpStatusCodeError
-}
-func fetchMusic() async throws -> ReturnedMusic {
-    var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
-    urlComponents.queryItems = [
-        "term": "j+cole",
-        "entity": "music"
-    ].map { URLQueryItem(name: $0.key, value: $0.value) }
-    urlComponents.queryItems?.append(urlComponents.queryItems)
-    let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
-    
-    guard let httpResponse = response as? HTTPURLResponse,
-          httpResponse.statusCode == 200 else { throw networkError.httpStatusCodeError }
-    return try JSONDecoder().decode(ReturnedMusic.self, from: data)
-}
-
+var queryDictionary: [String: String] = ["term": "j+cole", "media": "music", "limit": "5"]
+//var baseURL = URL(from: "https://itunes.apple.com/search")
+var urlComponents = URLComponents(string: "https://itunes.apple.com/search")!
+urlComponents.queryItems = queryDictionary.map( { URLQueryItem(name: $0.key, value: $0.value ) } )
 Task {
-    do {
-        try await fetchMusic()
-    } catch {
-        print(error.localizedDescription)
-    }
+  let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
+  if let httpResponse = response as? HTTPURLResponse,
+    httpResponse.statusCode == 200,
+    let string = String(data: data, encoding: .utf8) {
+    print(string)
+  }
 }
