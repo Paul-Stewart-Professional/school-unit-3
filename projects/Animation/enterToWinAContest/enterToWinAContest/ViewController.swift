8f0ed7ec7d30a5bbd6542f517e9f67c3e42552ec
Paul Stewart <paul.stewart.professional@gmail.com>
2023-12-20 10:29:12 -0700

    enter to win a contest done until button logic

---
+++ projects/Animation/enterToWinAContest/enterToWinAContest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  enterToWinAContest
+//
+//  Created by Paul Stewart on 12/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
+
