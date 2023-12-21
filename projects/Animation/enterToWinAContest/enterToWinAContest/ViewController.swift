0d3dc3b633b518f0dff4ca6c0a9c0947441469fe
Paul Stewart <paul.stewart.professional@gmail.com>
2023-12-20 22:27:42 -0700

    practical animation finished

--- projects/Animation/enterToWinAContest/enterToWinAContest/ViewController.swift
+++ projects/Animation/enterToWinAContest/enterToWinAContest/ViewController.swift
@@ -16,10 +16,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-    @IBAction func submitButtonTapped(_ sender: Any) {
-        
+    
+    
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if textField.text!.count == 9 {
+            performSegue(withIdentifier: "sucessfulSegue", sender: UIButton.self)
+        } else {
+            UIView.animate(withDuration: 0.2,
+                           delay: 0,
+                           options: [.autoreverse],
+                           animations: {
+                self.textField.transform = CGAffineTransform(translationX: 10, y: 0)
+            }) { (_) in
+                self.textField.transform = CGAffineTransform.identity
+                let feedbackGenerator = UINotificationFeedbackGenerator()
+                feedbackGenerator.notificationOccurred(.error)
+            }
+        }
     }
     
 }
